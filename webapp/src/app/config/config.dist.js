b939eb82a0ace04d2984fa3dd15f194e517674c6
Rémi SHARROCK <remi.sharrock@telecom-paristech.fr>
2017-11-06 02:29:33 -0500

    changing jor1kBaseFsUrl

--- webapp/src/app/config/config.dist.js
+++ webapp/src/app/config/config.dist.js
@@ -1,2 +1,2 @@
-export const jor1kBaseFsUrl = 'https://cs-education.github.io/sysassets/jor1kfs/sysroot/or1k/';
+export const jor1kBaseFsUrl = 'http://s-macke.github.io/jor1k/openrisc-sys/';
 export const jor1kWorkerUrl = 'jor1k-worker.js';
